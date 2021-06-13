f27ce00f6b4080d4057f4c7a9cad668b0a724aa4
lochanathiwanka <lochanathiwanka62@gmail.com>
2021-06-14 02:18:30 +0530

    Updated
    Signed-off-by: lochanathiwanka <lochanathiwanka62@gmail.com>

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,49 +1,15 @@
-/*document.getElementById('photo1').style.backgroundImage = "url('https://i.ibb.co/6gG9W4K/car1.jpg')";
-document.getElementById('photo2').style.backgroundImage = "url('https://i.ibb.co/SQdxKSF/car2.jpg')";
-document.getElementById('photo3').style.backgroundImage = "url('https://i.ibb.co/GM5RHcS/car3.jpg')";
-document.getElementById('photo4').style.backgroundImage = "url('https://i.ibb.co/Ph5K8zS/car4.jpg')";*/
+//sticky headbar navigation
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if (this.scrollY > 20) {
+            $('nav').addClass("sticky");
+        } else {
+            $('nav').removeClass("sticky");
+        }
+    });
+});
 
-/*
-let images = [
-    "https://i.ibb.co/6gG9W4K/car1.jpg",
-    "https://i.ibb.co/SQdxKSF/car2.jpg",
-    "https://i.ibb.co/GM5RHcS/car3.jpg",
-    "https://i.ibb.co/Ph5K8zS/car4.jpg"
-];
-
-let home = document.getElementById("home");
-
-let i = 0;
-setInterval(function() {
-    home.style.backgroundImage = "url(" + images[i] + ")";
-    i = i + 1;
-    if (i === images.length) {
-        i =  0;
-    }
-}, 10000);*/
-
-
-/*let images = [
-    "https://i.ibb.co/6gG9W4K/car1.jpg",
-    "https://i.ibb.co/SQdxKSF/car2.jpg",
-    "https://i.ibb.co/GM5RHcS/car3.jpg",
-    "https://i.ibb.co/Ph5K8zS/car4.jpg"
-];
-let i = 0;
-function changeBg() {
-    $('#home').css('background-image', 'url(' + images[i] + ')');
-    $('#home').fadeIn(10); //this is new, will fade in smoothly
-    i = i + 1;
-    if (i === images.length) {
-        i =  0;
-    }
-}
-function changeBackgroundSmoothly() {
-    $('#home').fadeOut(10, changeBg); //this is new, will fade out smoothly
-}
-setInterval(changeBackgroundSmoothly,5000);*/
-
-
+//load home background images
 let images = [
     "https://i.ibb.co/6gG9W4K/car1.jpg",
     "https://i.ibb.co/SQdxKSF/car2.jpg",
@@ -61,3 +27,50 @@ function nextBackground() {
 }
 
 setInterval(nextBackground, 8000);
+
+
+//password see change
+let condition = false;
+$('#password-logo').click(function () {
+    if (!condition) {
+        $(this).attr('class', 'far fa-eye');
+        condition = true;
+    } else {
+        $(this).attr('class', 'far fa-eye-slash');
+        condition = false;
+    }
+});
+
+//registration view enable and disable
+$('#btnRegister').click(function () {
+    $('#registration-popup').css("display", "flex");
+    $.ajax({
+        method: 'GET',
+        async: false,
+        url: './views/registrationFormImage.html',
+        contentType: 'text/html',
+        success: (data) => {
+            $('#registration-form-container').html(data);
+        }
+    });
+});
+$('#btn-close-registration-view').click(function () {
+    $('#registration-popup').css("display", "none");
+    $('#popup-container').css('height', '550px');
+});
+
+
+//customer registration form loading
+$('#btn-reg-as-customer').click(function () {
+    $('#popup-container').css('height', '690px');
+
+    $.ajax({
+        method: 'GET',
+        async: true,
+        url: './views/customerRegistration.html',
+        contentType: 'text/html',
+        success: (data) => {
+            $('#registration-form-container').html(data);
+        }
+    });
+});
